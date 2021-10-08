2e33dd0d8dc6857db097838fbc74c1017edc63ae
student <student@lab.pl.wsei>
2021-10-08 11:48:40 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldpaszkiewicz/HelloWorldPaszkiewicz.java
@@ -0,0 +1,21 @@
+package pl.vistula.helloworldpaszkiewicz;
+
+
+
+
+
+public class HelloWorldPaszkiewicz {
+
+    public static void main(String[] Ars)
+    {
+
+        System.out.println("Hello Vistula Paszkiewicz");
+
+
+
+
+
+    }
+
+
+}
